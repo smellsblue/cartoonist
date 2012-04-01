4da89d692475d285dacd032669522ed9886b29b1
Mike Stone <stonemj@gmail.com>
2012-03-31 19:38:54 -0700

    A bunch of specs, and a bunch of functionality for config values.

--- app/models/cartoonist_config.rb
+++ app/models/cartoonist_config.rb
@@ -1,2 +1,89 @@
 class CartoonistConfig < ActiveRecord::Base
+  class << self
+    def [](label)
+      raise "Invalid label" unless label.present?
+      raise "Missing config definition for '#{label}'" unless Meta[label.to_sym]
+      record = where(:label => label.to_s).first
+
+      if record
+        Meta[label.to_sym].convert record.value
+      else
+        Meta[label.to_sym].default
+      end
+    end
+
+    def []=(label, value)
+      raise "Invalid label" unless label.present?
+      raise "Missing config definition for '#{label}'" unless Meta[label.to_sym]
+      record = where(:label => label.to_s).first
+      record = CartoonistConfig.new :label => label.to_s unless record
+      record.value = Meta[label.to_sym].convert_to_save value
+      record.save!
+      value
+    end
+
+    def define(label, options = {})
+      Meta[label] = Meta.new label, options
+    end
+  end
+
+  class Meta
+    attr_reader :label, :type, :default
+    @@configs = {}
+
+    def initialize(label, options)
+      @label = label.to_sym
+      @type = (options[:type] || :string).to_sym
+      raise "Invalid config type #{@type}" unless [:string, :boolean, :int, :float].include? @type
+
+      if options.include? :default
+        @default = options[:default]
+      elsif @type == :string
+        @default = ""
+      elsif @type == :boolean
+        @default = false
+      elsif @type == :int
+        @default = 0
+      elsif @type == :float
+        @default = 0.0
+      end
+    end
+
+    def convert(value)
+      return default if value.nil?
+
+      case type
+      when :string
+        value
+      when :boolean
+        value == "true"
+      when :int
+        value.to_i
+      when :float
+        value.to_f
+      end
+    end
+
+    def convert_to_save(value)
+      return value if value.nil?
+
+      if type == :boolean
+        (!!value).to_s
+      else
+        value.to_s
+      end
+    end
+
+    class << self
+      def [](label)
+        raise "Missing config '#{label}'" unless @@configs[label.to_sym]
+        @@configs[label.to_sym]
+      end
+
+      def []=(label, definition)
+        raise "Duplicate configuration '#{label}' given!" if @@configs[label.to_sym]
+        @@configs[label.to_sym] = definition
+      end
+    end
+  end
 end
---
+++ spec/models/cartoonist_config_spec.rb
@@ -0,0 +1,70 @@
+require "spec_helper"
+
+describe CartoonistConfig do
+  before { CartoonistConfig::Meta.class_variable_set :@@configs, {} }
+
+  it "fails on missing meta" do
+    lambda { CartoonistConfig[:missing_config] }.should raise_error
+    lambda { CartoonistConfig::Meta[:missing_config] }.should raise_error
+  end
+
+  it "supports int types" do
+    CartoonistConfig.define :test_config, :type => :int
+    CartoonistConfig::Meta[:test_config].type.should == :int
+    CartoonistConfig[:test_config] = 5
+    CartoonistConfig[:test_config].should == 5
+  end
+
+  it "supports boolean types" do
+    CartoonistConfig.define :test_config, :type => :boolean
+    CartoonistConfig::Meta[:test_config].type.should == :boolean
+    CartoonistConfig[:test_config] = 1
+    CartoonistConfig[:test_config].should == true
+    CartoonistConfig[:test_config] = nil
+    CartoonistConfig[:test_config].should == false
+    CartoonistConfig[:test_config] = false
+    CartoonistConfig[:test_config].should == false
+  end
+
+  it "supports float types" do
+    CartoonistConfig.define :test_config, :type => :float
+    CartoonistConfig::Meta[:test_config].type.should == :float
+    CartoonistConfig[:test_config] = 4.2
+    CartoonistConfig[:test_config].should == 4.2
+  end
+
+  it "supports string types" do
+    CartoonistConfig.define :test_config, :type => :string
+    CartoonistConfig::Meta[:test_config].type.should == :string
+    CartoonistConfig[:test_config] = "This is a test"
+    CartoonistConfig[:test_config].should == "This is a test"
+  end
+
+  it "defaults to string types" do
+    CartoonistConfig.define :test_config
+    CartoonistConfig::Meta[:test_config].type.should == :string
+    CartoonistConfig[:test_config] = "This is a test"
+    CartoonistConfig[:test_config].should == "This is a test"
+  end
+
+  it "has default values for all types" do
+    CartoonistConfig.define :test_config_string, :type => :string
+    CartoonistConfig.define :test_config_boolean, :type => :boolean
+    CartoonistConfig.define :test_config_int, :type => :int
+    CartoonistConfig.define :test_config_float, :type => :float
+    CartoonistConfig[:test_config_string].should == ""
+    CartoonistConfig[:test_config_boolean].should == false
+    CartoonistConfig[:test_config_int].should == 0
+    CartoonistConfig[:test_config_float].should == 0.0
+  end
+
+  it "disallows multiple config values of the same label" do
+    CartoonistConfig.define :test_config
+    lambda { CartoonistConfig.define :test_config }.should raise_error
+  end
+
+  it "supports explicit default values" do
+    CartoonistConfig.define :test_config, :default => "This is the default"
+    CartoonistConfig[:test_config].should == "This is the default"
+  end
+end
