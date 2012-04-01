4802ad2cd8e48b84eec6cb2cd7987d62582289ea
Mike Stone <stonemj@gmail.com>
2012-04-01 02:28:26 -0700

    Define the settings all the time when in development mode.

--- config/initializers/setting_definitions.rb
+++ config/initializers/setting_definitions.rb
@@ -1,40 +1,49 @@
-if Setting.table_exists?
-  Setting.define :domain
-  Setting.define :site_name
-  Setting.define :site_heading
-  Setting.define :site_update_description
-  Setting.define :theme, :type => :symbol
-  Setting.define :schedule, :type => :array, :default => [:monday, :wednesday, :friday]
-  Setting.define :copyright_starting_year, :type => :int
-  Setting.define :copyright_owners
-  Setting.define :default_title
-  Setting.define :default_tweet
-  Setting.define :twitter_enabled, :type => :boolean
-  Setting.define :twitter_handle
-  Setting.define :twitter_consumer_key
-  Setting.define :twitter_consumer_secret
-  Setting.define :twitter_oath_token
-  Setting.define :twitter_oath_token_secret
-  Setting.define :google_analytics_enabled, :type => :boolean
-  Setting.define :google_analytics_account
-  Setting.define :disqus_enabled, :type => :boolean
-  Setting.define :disqus_shortname
-  Setting.define :disqus_comic_category
-  Setting.define :disqus_blog_post_category
-  Setting.define :disqus_page_category
-  Setting.define :admin_users, :type => :hash
-  Setting.define :secret_token
+Rails.application.config.to_prepare do
+  if Setting.table_exists?
+    secret_token_changed = lambda do
+      # Your secret key for verifying the integrity of signed cookies.
+      # If you change this key, all old signed cookies will become invalid!
+      # Make sure the secret is at least 30 characters and all random,
+      # no regular words or you'll be exposed to dictionary attacks.
+      Cartoonist::Application.config.secret_token = Setting[:secret_token]
+    end
 
-  Twitter.configure do |config|
-    config.consumer_key = Setting[:twitter_consumer_key]
-    config.consumer_secret = Setting[:twitter_consumer_secret]
-    config.oauth_token = Setting[:twitter_oath_token]
-    config.oauth_token_secret = Setting[:twitter_oath_token_secret]
+    twitter_auth_changed = lambda do
+      Twitter.configure do |config|
+        config.consumer_key = Setting[:twitter_consumer_key]
+        config.consumer_secret = Setting[:twitter_consumer_secret]
+        config.oauth_token = Setting[:twitter_oath_token]
+        config.oauth_token_secret = Setting[:twitter_oath_token_secret]
+      end
+    end
+
+    Setting.define :domain
+    Setting.define :site_name
+    Setting.define :site_heading
+    Setting.define :site_update_description
+    Setting.define :theme, :type => :symbol
+    Setting.define :schedule, :type => :array, :default => [:monday, :wednesday, :friday]
+    Setting.define :copyright_starting_year, :type => :int
+    Setting.define :copyright_owners
+    Setting.define :default_title
+    Setting.define :default_tweet
+    Setting.define :twitter_enabled, :type => :boolean
+    Setting.define :twitter_handle
+    Setting.define :twitter_consumer_key, :onchange => twitter_auth_changed
+    Setting.define :twitter_consumer_secret, :onchange => twitter_auth_changed
+    Setting.define :twitter_oath_token, :onchange => twitter_auth_changed
+    Setting.define :twitter_oath_token_secret, :onchange => twitter_auth_changed
+    Setting.define :google_analytics_enabled, :type => :boolean
+    Setting.define :google_analytics_account
+    Setting.define :disqus_enabled, :type => :boolean
+    Setting.define :disqus_shortname
+    Setting.define :disqus_comic_category
+    Setting.define :disqus_blog_post_category
+    Setting.define :disqus_page_category
+    Setting.define :admin_users, :type => :hash
+    Setting.define :secret_token, :default => "ThisTokenMustBeRegenerated....", :onchange => secret_token_changed
+
+    secret_token_changed.call
+    twitter_auth_changed.call
   end
-
-  # Your secret key for verifying the integrity of signed cookies.
-  # If you change this key, all old signed cookies will become invalid!
-  # Make sure the secret is at least 30 characters and all random,
-  # no regular words or you'll be exposed to dictionary attacks.
-  Cartoonist::Application.config.secret_token = Setting[:secret_token]
 end
