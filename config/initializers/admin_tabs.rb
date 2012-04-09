39735d1ae38ec6d7dd87d00f0c05843135eb60c7
Mike Stone <stonemj@gmail.com>
2012-04-08 22:58:07 -0700

    Update localisation.

--- config/initializers/admin_tabs.rb
+++ config/initializers/admin_tabs.rb
@@ -1,6 +1,6 @@
 Rails.application.config.to_prepare do
   Cartoonist::Admin::Tab.add :comics, "/comic_admin"
-  Cartoonist::Admin::Tab.add :blog_posts, "/blog_admin"
+  Cartoonist::Admin::Tab.add :blog, "/blog_admin"
   Cartoonist::Admin::Tab.add :pages, "/page_admin"
   Cartoonist::Admin::Tab.add :general, "/admin"
 end
