9ecc01e7e173fd01077f28d41f7bef535508aea6
Mike Stone <stonemj@gmail.com>
2012-04-10 01:27:18 -0700

    Stub for locale, and add admin tab.

--- lib/cartoonist-announcements/engine.rb
+++ lib/cartoonist-announcements/engine.rb
@@ -1,5 +1,6 @@
 module CartoonistAnnouncements
   class Engine < ::Rails::Engine
+    Cartoonist::Admin::Tab.add :announcements, :url => "/announcements"
     Cartoonist::Asset.include_js "cartoonist-announcements.js"
   end
 end
