10950d93c8ecc5126aa45a8807b0fbb7ec60fc2f
Mike Stone <stonemj@gmail.com>
2012-04-12 00:27:21 -0700

    Update to use simpler migration api, and add backup hook for announcements.

--- lib/cartoonist-announcements/engine.rb
+++ lib/cartoonist-announcements/engine.rb
@@ -2,7 +2,10 @@ module CartoonistAnnouncements
   class Engine < ::Rails::Engine
     Cartoonist::Admin::Tab.add :announcements, :url => "/announcements_admin"
     Cartoonist::Asset.include_js "cartoonist-announcements.js"
-    Cartoonist::Migration.add_dirs self.paths["db/migrate"].existent
+    Cartoonist::Migration.add_for self
+    Cartoonist::Backup.for :announcements do
+      Announcement.order(:id).all
+    end
     Cartoonist::Routes.add do
       resources :announcements
       resources :announcements_admin
