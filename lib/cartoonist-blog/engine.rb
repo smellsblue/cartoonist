435e9028c767b7344fd61410fae6b91b96396723
Mike Stone <stonemj@gmail.com>
2012-04-13 00:53:34 -0700

    Migrated some more configuration, and the migration from cartoonist.

--- lib/cartoonist-blog/engine.rb
+++ lib/cartoonist-blog/engine.rb
@@ -1,4 +1,32 @@
 module CartoonistBlog
   class Engine < ::Rails::Engine
+    Cartoonist::Admin::Tab.add :blog, :url => "/blog_admin", :order => 1
+    Cartoonist::Migration.add_for self
+
+    Cartoonist::Backup.for :blog_posts do
+      BlogPost.order(:id).all
+    end
+
+    Cartoonist::Routes.add do
+      resources :blog do
+        collection do
+          get "archives"
+          get "feed", :defaults => { :format => "rss" }
+        end
+      end
+
+      resources :blog_admin do
+        member do
+          post "lock"
+          get "preview"
+          post "unlock"
+        end
+
+        collection do
+          get "preview"
+          post "preview_content"
+        end
+      end
+    end
   end
 end
