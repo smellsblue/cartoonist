7afdbdc3fa760130583cfbb6d1450cf8a5194198
Mike Stone <stonemj@gmail.com>
2012-04-14 01:08:00 -0700

    Move most of the functionality off to the core cartoonist gem.

--- db/migrate/20120320043253_create_database_files.rb
+++
@@ -1,9 +0,0 @@
-class CreateDatabaseFiles < ActiveRecord::Migration
-  def change
-    create_table :database_files do |t|
-      t.string :filename
-      t.binary :content, :null => false
-      t.timestamps
-    end
-  end
-end
