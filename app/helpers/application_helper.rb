f958ebec023dfe326b5fd597fdc76f036b9e0a9b
Mike Stone <stonemj@gmail.com>
2012-04-15 18:00:11 -0700

    Removed helpers that are now in the gem.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -85,30 +85,6 @@ module ApplicationHelper
     end
   end
 
-  def comic_url(number)
-    if preview?
-      "/comic_admin/#{number}/preview"
-    else
-      "/#{number}"
-    end
-  end
-
-  def comic_img_url
-    if preview?
-      @comic.preview_img_url
-    else
-      @comic.img_url
-    end
-  end
-
-  def random_url
-    if preview?
-      "/comic_admin/preview_random"
-    else
-      "/random"
-    end
-  end
-
   def content_license_url
     "http://creativecommons.org/licenses/by-nc/3.0/"
   end
