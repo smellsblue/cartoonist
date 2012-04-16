381069113c4e1f85f90a18add12701a2c424b3c3
Mike Stone <stonemj@gmail.com>
2012-04-15 17:00:59 -0700

    Move social links into the gems.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,10 +40,6 @@ module ApplicationHelper
     @disqus_options = options
   end
 
-  def show_social_links?
-    @comic || @post
-  end
-
   def preview?
     @for_preview
   end
@@ -105,77 +101,5 @@ module ApplicationHelper
     copyright_years = Setting[:copyright_starting_year].to_s
     copyright_years = "#{copyright_years}-#{year}" if year != copyright_years
     "&copy; #{h copyright_years} #{h Setting[:copyright_owners]}".html_safe
-  end
-
-  def rss_path
-    if @post
-      "/blog/feed"
-    else
-      "/feed"
-    end
-  end
-
-  def rss_title
-    if @post
-      t "application.layout.social.rss_blog_title", :site_name => Setting[:site_name]
-    else
-      t "application.layout.social.rss_comic_title", :site_name => Setting[:site_name]
-    end
-  end
-
-  def facebook_link_title
-    if @comic
-      t "application.layout.social.facebook_comic_title"
-    else
-      t "application.layout.social.facebook_blog_title"
-    end
-  end
-
-  def twitter_link_title
-    if @comic
-      t "application.layout.social.twitter_comic_title"
-    else
-      t "application.layout.social.twitter_blog_title"
-    end
-  end
-
-  def google_plus_link_title
-    if @comic
-      t "application.layout.social.google_plus_comic_title"
-    else
-      t "application.layout.social.google_plus_blog_title"
-    end
-  end
-
-  def share_facebook_text_message
-    if @comic
-      t "application.layout.social.facebook_comic_text_message", :title => @comic.title
-    else
-      t "application.layout.social.facebook_blog_text_message", :title => @post.title
-    end
-  end
-
-  def share_facebook_url_message
-    if @comic
-      "http://#{Setting[:domain]}/#{@comic.number}"
-    else
-      "http://#{Setting[:domain]}/blog/#{@post.url_title}"
-    end
-  end
-
-  def share_twitter_message
-    if @comic
-      t "application.layout.social.twitter_comic_text_message", :title => @comic.title, :url => "http://#{Setting[:domain]}/#{@comic.number}"
-    else
-      t "application.layout.social.twitter_blog_text_message", :url => "http://#{Setting[:domain]}/blog/#{@post.url_title}"
-    end
-  end
-
-  def share_google_plus_message
-    if @comic
-      "http://#{Setting[:domain]}/#{@comic.number}"
-    else
-      "http://#{Setting[:domain]}/blog/#{@post.url_title}"
-    end
   end
 end
