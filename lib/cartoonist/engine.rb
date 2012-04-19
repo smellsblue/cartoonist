411b76c03fdfcba15b976bfae613c8849562bfbd
Mike Stone <stonemj@gmail.com>
2012-04-18 21:05:24 -0700

    Add in mime type for favicons.

--- lib/cartoonist/engine.rb
+++ lib/cartoonist/engine.rb
@@ -312,6 +312,7 @@ module Cartoonist
       end
     end
 
+    Mime::Type.register "image/x-icon", :ico
     Cartoonist::Admin::Tab.add :general, :url => "/admin", :order => 3
     Cartoonist::Navigation::Link.add :url => (lambda { "https://twitter.com/#{Setting[:twitter_handle]}" }), :class => "follow-us", :label => "application.layout.navigation.follow_on_twitter", :title => "application.layout.navigation.follow_on_twitter_title", :order => 2
     Cartoonist::Migration.add_for self
