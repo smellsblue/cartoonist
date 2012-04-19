0c40ff368582d517c0ef6b8440f0accb005df8a4
Mike Stone <stonemj@gmail.com>
2012-04-19 01:33:38 -0700

    Don't rely on there being anything in the given feed.

--- app/models/blog_feed.rb
+++ app/models/blog_feed.rb
@@ -2,7 +2,15 @@ class BlogFeed
   attr_reader :pub_date, :items
 
   def initialize(feed)
-    @pub_date = feed.first.posted_at.localtime.strftime "%a, %d %b %Y %H:%M:00 %z"
+    first = feed.first
+
+    if first
+      first_date = first.posted_at
+    else
+      first_date = Time.now
+    end
+
+    @pub_date = first_date.localtime.strftime "%a, %d %b %Y %H:%M:00 %z"
     @items = feed.map do |item|
       BlogFeed::Item.new item
     end
