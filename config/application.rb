e792b629a6be01e069a6a6db36588e83ac6f0190
Mike Stone <stonemj@gmail.com>
2012-04-18 21:55:48 -0700

    Not including helpers is now done in the gem.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ end
 
 module Cartoonist
   class Application < Rails::Application
-    config.action_controller.include_all_helpers = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
