29fef3e10feecadd3b66e0ccf7458c21d6cb5b01
Mike Stone <stonemj@gmail.com>
2012-04-21 02:11:27 -0700

    Add config for the devise parent controller.

--- lib/cartoonist/engine.rb
+++ lib/cartoonist/engine.rb
@@ -3,6 +3,8 @@ module Cartoonist
     # Use this hook to configure devise mailer, warden hooks and so forth.
     # Many of these configuration options can be set straight in your model.
     Devise.setup do |devise_config|
+      devise_config.parent_controller = "CartoonistController"
+
       # ==> Mailer Configuration
       # Configure the e-mail address which will be shown in Devise::Mailer,
       # note that it will be overwritten if you use your own mailer class with default "from" parameter.
