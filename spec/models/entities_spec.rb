82435cab9794dfa980a251c5c334b68ebb9c0d63
Mike Stone <stonemj@gmail.com>
2012-05-07 23:21:15 -0700

    New tests for entity urls.

---
+++ spec/models/entities_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+describe Entity do
+  describe "entity url methods" do
+    before do
+      Setting.stub(:[]).with(:domain).and_return("testdomain.com")
+    end
+
+    it "supports the global entity_absolute_url and entity_relative_url methods" do
+      BlogPost.entity_relative_url.should == "/blog"
+      BlogPost.entity_absolute_url.should == "http://testdomain.com/blog"
+      Comic.entity_relative_url.should == "/comic"
+      Comic.entity_absolute_url.should == "http://testdomain.com/comic"
+      Page.entity_relative_url.should == nil
+      Page.entity_absolute_url.should == nil
+    end
+
+    it "supports the instance entity_absolute_url and entity_relative_url methods" do
+      BlogPost.new(:url_title => "example-blog-post").entity_relative_url.should == "/blog/example-blog-post"
+      BlogPost.new(:url_title => "example-blog-post").entity_absolute_url.should == "http://testdomain.com/blog/example-blog-post"
+      Comic.new(:number => 42).entity_relative_url.should == "/comic/42"
+      Comic.new(:number => 42).entity_absolute_url.should == "http://testdomain.com/comic/42"
+      Page.new(:path => "example-path").entity_relative_url.should == "/example-path"
+      Page.new(:path => "example-path").entity_absolute_url.should == "http://testdomain.com/example-path"
+    end
+  end
+end
