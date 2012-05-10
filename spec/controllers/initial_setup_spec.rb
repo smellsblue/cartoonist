afec16fb4238bb69e3d1962af773bf919d007889
Mike Stone <stonemj@gmail.com>
2012-05-10 00:10:46 -0700

    Update specs

--- spec/controllers/initial_setup_spec.rb
+++ spec/controllers/initial_setup_spec.rb
@@ -4,18 +4,18 @@ describe AdminController do
   it "redirects to initial setup when an admin page is loaded and there are no users" do
     User.should_receive(:count).and_return(0)
     get :show
-    response.should redirect_to("/settings/initial_setup")
+    response.should redirect_to("/admin/settings/initial_setup")
   end
 
   it "doesn't redirect to initial setup if there are users" do
     User.should_receive(:count).and_return(1)
     controller.should_receive(:authenticate_user!).and_return(true)
     get :show
-    response.should_not redirect_to("/settings/initial_setup")
+    response.should_not redirect_to("/admin/settings/initial_setup")
   end
 end
 
-describe SettingsController do
+describe Admin::SettingsController do
   it "doesn't allow going to the initial setup if there are users" do
     User.should_receive(:count).and_return(1)
     get :initial_setup
@@ -36,7 +36,7 @@ describe SettingsController do
     it "fails if the same password is not given" do
       post :save_initial_setup, :domain => "testing.com", :site_name => "Testing", :admin_email => "user@example.com", :admin_password => "test123", :admin_confirm_password => "test321", :admin_name => "test"
       User.count.should == 0
-      response.should redirect_to("/settings/initial_setup")
+      response.should redirect_to("/admin/settings/initial_setup")
       flash[:error].should be
     end
 
