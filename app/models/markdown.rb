0b0fab816e573a5c54ad2ca12f7061eb0eb7b2e7
Mike Stone <stonemj@gmail.com>
2012-05-22 19:14:55 -0700

    Support translating relative paths to absolute paths.

--- app/models/markdown.rb
+++ app/models/markdown.rb
@@ -1,13 +1,33 @@
 class Markdown
+  class LinkToAbsoluteRenderer < Redcarpet::Render::HTML
+    def link(link, title, content)
+      link = "http://#{Setting[:domain]}#{link}" if link =~ /^\//
+      title = %{ title="#{title}"} if title
+      %{<a href="#{link}"#{title}>#{content}</a>}
+    end
+  end
+
   class << self
-    def config
-      @markdown ||= Redcarpet::Markdown.new Redcarpet::Render::HTML
+    RENDER_DEFAULTS = { :html_safe => true, :link_to_absolute => false }
+
+    def standard_renderer
+      @standard_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     end
 
-    def render(text, safe = true)
-      result = config.render text
+    def link_to_absolute_renderer
+      @link_to_absolute_renderer ||= Redcarpet::Markdown.new(LinkToAbsoluteRenderer.new)
+    end
 
-      if safe
+    def render(text, options = {})
+      options = RENDER_DEFAULTS.merge options
+
+      if options[:link_to_absolute]
+        result = link_to_absolute_renderer.render text
+      else
+        result = standard_renderer.render text
+      end
+
+      if options[:html_safe]
         result.html_safe
       else
         result
