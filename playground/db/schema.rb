fb6f99effd8ebba4eab2eb3da0fbe70b6e321fef
Mike Virata-Stone <stonemj@gmail.com>
2013-02-13 23:14:35 -0800

    Commit schema changes.

--- playground/db/schema.rb
+++ playground/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121227022810) do
+ActiveRecord::Schema.define(:version => 20130214061140) do
 
   create_table "announcements", :force => true do |t|
     t.datetime "posted_at"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20121227022810) do
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
     t.boolean  "locked",     :default => false, :null => false
+    t.integer  "site_id",    :default => 1,     :null => false
   end
 
   add_index "announcements", ["expired_at"], :name => "index_announcements_on_expired_at"
@@ -36,11 +37,12 @@ ActiveRecord::Schema.define(:version => 20121227022810) do
     t.boolean  "locked",     :default => false, :null => false
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
+    t.integer  "site_id",    :default => 1,     :null => false
   end
 
   add_index "blog_posts", ["posted_at"], :name => "index_blog_posts_on_posted_at"
-  add_index "blog_posts", ["title"], :name => "index_blog_posts_on_title", :unique => true
-  add_index "blog_posts", ["url_title"], :name => "index_blog_posts_on_url_title", :unique => true
+  add_index "blog_posts", ["title", "site_id"], :name => "index_blog_posts_on_title_and_site_id", :unique => true
+  add_index "blog_posts", ["url_title", "site_id"], :name => "index_blog_posts_on_url_title_and_site_id", :unique => true
 
   create_table "comics", :force => true do |t|
     t.integer  "number",                               :null => false
@@ -54,28 +56,31 @@ ActiveRecord::Schema.define(:version => 20121227022810) do
     t.boolean  "locked",            :default => false, :null => false
     t.datetime "created_at",                           :null => false
     t.datetime "updated_at",                           :null => false
+    t.integer  "site_id",           :default => 1,     :null => false
   end
 
-  add_index "comics", ["number"], :name => "index_comics_on_number", :unique => true
+  add_index "comics", ["number", "site_id"], :name => "index_comics_on_number_and_site_id", :unique => true
   add_index "comics", ["posted_at"], :name => "index_comics_on_posted_at"
 
   create_table "database_files", :force => true do |t|
     t.string   "filename"
-    t.binary   "content",    :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.binary   "content",                   :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
     t.string   "extension"
+    t.integer  "site_id",    :default => 1, :null => false
   end
 
   create_table "entity_tags", :force => true do |t|
-    t.integer  "entity_id",   :null => false
-    t.string   "entity_type", :null => false
-    t.integer  "tag_id",      :null => false
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer  "entity_id",                  :null => false
+    t.string   "entity_type",                :null => false
+    t.integer  "tag_id",                     :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "site_id",     :default => 1, :null => false
   end
 
-  add_index "entity_tags", ["entity_id", "entity_type", "tag_id"], :name => "index_entity_tags_on_entity_id_and_entity_type_and_tag_id", :unique => true
+  add_index "entity_tags", ["entity_id", "entity_type", "tag_id", "site_id"], :name => "index_entity_tags_on_entity_id__entity_type__tag_id__site_id", :unique => true
 
   create_table "pages", :force => true do |t|
     t.string   "title",                         :null => false
@@ -87,10 +92,11 @@ ActiveRecord::Schema.define(:version => 20121227022810) do
     t.boolean  "in_sitemap", :default => false, :null => false
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
+    t.integer  "site_id",    :default => 1,     :null => false
   end
 
-  add_index "pages", ["path"], :name => "index_pages_on_path", :unique => true
-  add_index "pages", ["title"], :name => "index_pages_on_title", :unique => true
+  add_index "pages", ["path", "site_id"], :name => "index_pages_on_path_and_site_id", :unique => true
+  add_index "pages", ["title", "site_id"], :name => "index_pages_on_title_and_site_id", :unique => true
 
   create_table "settings", :force => true do |t|
     t.string   "label",                         :null => false
@@ -98,9 +104,20 @@ ActiveRecord::Schema.define(:version => 20121227022810) do
     t.boolean  "locked",     :default => false, :null => false
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
+    t.integer  "site_id",    :default => 1,     :null => false
   end
 
-  add_index "settings", ["label"], :name => "index_settings_on_label", :unique => true
+  add_index "settings", ["label", "site_id"], :name => "index_settings_on_label_and_site_id", :unique => true
+
+  create_table "sites", :force => true do |t|
+    t.string   "name",                          :null => false
+    t.text     "description"
+    t.boolean  "enabled",     :default => true, :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+  end
+
+  add_index "sites", ["name"], :name => "index_sites_on_name", :unique => true
 
   create_table "suggestions", :force => true do |t|
     t.string   "name"
@@ -111,26 +128,29 @@ ActiveRecord::Schema.define(:version => 20121227022810) do
     t.boolean  "hidden",     :default => false, :null => false
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
+    t.integer  "site_id",    :default => 1,     :null => false
   end
 
   create_table "tags", :force => true do |t|
-    t.string   "label",      :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "label",                     :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "site_id",    :default => 1, :null => false
   end
 
-  add_index "tags", ["label"], :name => "index_tags_on_label", :unique => true
+  add_index "tags", ["label", "site_id"], :name => "index_tags_on_label_and_site_id", :unique => true
 
   create_table "tweets", :force => true do |t|
-    t.integer  "entity_id",   :null => false
-    t.string   "entity_type", :null => false
-    t.string   "tweet",       :null => false
+    t.integer  "entity_id",                  :null => false
+    t.string   "entity_type",                :null => false
+    t.string   "tweet",                      :null => false
     t.datetime "tweeted_at"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "site_id",     :default => 1, :null => false
   end
 
-  add_index "tweets", ["entity_id", "entity_type"], :name => "index_tweets_on_entity_id_and_entity_type", :unique => true
+  add_index "tweets", ["entity_id", "entity_type", "site_id"], :name => "index_tweets_on_entity_id_and_entity_type_and_site_id", :unique => true
   add_index "tweets", ["tweeted_at"], :name => "index_tweets_on_tweeted_at"
 
   create_table "users", :force => true do |t|
