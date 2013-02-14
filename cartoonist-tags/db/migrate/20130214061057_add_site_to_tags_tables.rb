81fcb86817bad2e8eb5ecc459027d90083dd3093
Mike Virata-Stone <stonemj@gmail.com>
2013-02-13 23:12:06 -0800

    Use name for index that is too long of a name.

--- cartoonist-tags/db/migrate/20130214061057_add_site_to_tags_tables.rb
+++ cartoonist-tags/db/migrate/20130214061057_add_site_to_tags_tables.rb
@@ -5,12 +5,12 @@ class AddSiteToTagsTables < ActiveRecord::Migration
     remove_index :tags, :label
     remove_index :entity_tags, [:entity_id, :entity_type, :tag_id]
     add_index :tags, [:label, :site_id], :unique => true
-    add_index :entity_tags, [:entity_id, :entity_type, :tag_id, :site_id], :unique => true
+    add_index :entity_tags, [:entity_id, :entity_type, :tag_id, :site_id], :unique => true, :name => "index_entity_tags_on_entity_id__entity_type__tag_id__site_id"
   end
 
   def down
     remove_index :tags, [:label, :site_id]
-    remove_index :entity_tags, [:entity_id, :entity_type, :tag_id, :site_id]
+    remove_index :entity_tags, :name => "index_entity_tags_on_entity_id__entity_type__tag_id__site_id"
     add_index :tags, :label, :unique => true
     add_index :entity_tags, [:entity_id, :entity_type, :tag_id], :unique => true
     remove_column :tags, :site_id
