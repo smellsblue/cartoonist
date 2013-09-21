c354d468f26dd0318fe55e1e51d6fea94efe9463
Mike Virata-Stone <mike@virata-stone.com>
2013-09-21 14:16:09 -0700

    Update version for the new patch.

--- cartoonist-announcements/lib/cartoonist-announcements/version.rb
+++ cartoonist-announcements/lib/cartoonist-announcements/version.rb
@@ -2,7 +2,7 @@ module CartoonistAnnouncements
   class Version
     class << self
       def to_s
-        "0.0.19"
+        "0.0.19.3"
       end
     end
   end
--- cartoonist-default-theme/lib/cartoonist-default-theme/version.rb
+++ cartoonist-default-theme/lib/cartoonist-default-theme/version.rb
@@ -2,7 +2,7 @@ module CartoonistDefaultTheme
   class Version
     class << self
       def to_s
-        "0.0.19"
+        "0.0.19.3"
       end
     end
   end
